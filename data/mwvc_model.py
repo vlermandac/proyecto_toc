5194a029898a067b970535a6aeef4c9b4212c471
vlermandac <vicentelcandia@gmail.com>
2023-01-05 18:12:49 -0300

    mwvc model updated

--- data/mwvc_model.py
+++ data/mwvc_model.py
@@ -1,15 +1,17 @@
 # Model generator for mwvc in scip
+# Also writes a file in a MIP model format
 
 from pyscipopt import Model
 import numpy as np
 
-def mwvc(graph, weights):
+# A NetworkX generated graph is assumed
+def mwvc(graph, weights, filename):
     model = Model()
 
-    vertices = graph.vertices
-    edges = graph.edges
+    vertices = np.array(graph.nodes)
+    edges = np.array(graph.edges)
     x = np.array()
-    w = np.array()
+    w = np.array(weights)
 
     # decision variables
     for v in vertices:
@@ -20,6 +22,14 @@ def mwvc(graph, weights):
 
     # constraints
     for u,v in edges:
-      model.addCons(quicksum(x[u], x[v]) >= 1)
+        model.addCons(quicksum(x[u], x[v]) >= 1)
+
+    # writes to output file
+    with open(filename, 'w') as lp_file:
+        lp_file.write("maximize\nOBJ:" + "".join([f" + 1 x{node+1}" for node in range(len(graph))]) + "\n")
+        lp_file.write("\nsubject to\n")
+        for count, group in enumerate(inequalities):
+            lp_file.write(f"C{count+1}:" + "".join([f" + x{node+1}" for node in sorted(group)]) + " <= 1\n")
+        lp_file.write("\nbinary\n" + " ".join([f"x{node+1}" for node in range(len(graph))]) + "\n")
 
 
